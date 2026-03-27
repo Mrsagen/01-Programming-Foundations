d5da47c7ee7a23d165d5b37e49e19448959cf73b
Fredrik Sagen <fredrik.a.sagen@gmail.com>
2026-03-27 15:47:27 +0100

    activity

---
+++ AW02 - Module 2. Strings, Functions, Async and OOP/2.1. Strings and string manipulation Introduction/Activity strings in JavaScript/activity.js
@@ -0,0 +1,14 @@
+/*
+Write a script that does the following:
+
+1. Create a string that contains your full name (first and last).
+2. Split the name into an array using a space as the separator.
+3. Access and log only the first name using array indexing.
+4. Join the array back into a single string using a custom separator, like -.
+5. Log the length of the full name string (how many characters it has, including the space).
+6. Log the first and last character of the original name string using indexing.
+*/
+
+let myname = "Fredrik Sagen";
+
+const splitname = myname.split(' ');
--- AW02 - Module 2. Strings, Functions, Async and OOP/2.1. Strings and string manipulation Introduction/strings.js
+++ AW02 - Module 2. Strings, Functions, Async and OOP/2.1. Strings and string manipulation Introduction/strings.js
@@ -4,3 +4,5 @@ let rawMessage = 'hello! i need HELP. i repeat, I need HELP!! please respond. ';
 console.log(rawName);
 console.log(rawMessage);
 
+const nameChars = rawName.split('');
+console.log(nameChars);
